8d25bdde8e0d7ccf8d0c6ea9c141575f52caee0b
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2025-07-23 12:19:42 -0400

    add shebang to script, add help to argument, and turn it into a package
    the packaging might not be done correctly...

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='timelinechart',
+    version='0.0.0',
+    description="A timeline chart tool",
+    install_requires=['matplotlib'],
+    scripts=['timelinechart'],
+)
